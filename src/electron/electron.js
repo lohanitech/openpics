e8124e3b7a394454b05c23201e485a8178863c4a
Damodar Lohani <dlohani48@gmail.com>
2017-02-25 13:40:15 +0545

    updated for use with electron-builder

--- src/electron/electron.js
+++ src/electron/electron.js
@@ -16,7 +16,7 @@ function createWindow () {
  
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, '/../index.html'),
+    pathname: path.join(__dirname, '/index.html'),
     protocol: 'file:',
     slashes: true
   }));
