b5a7b93f10f4ccadb53096bf543c8f25f64e51cf
Damodar Lohani <dlohani48@gmail.com>
2017-02-28 09:18:23 +0545

    edit collection function

--- src/app/services/local-store.ts
+++ src/app/services/local-store.ts
@@ -93,6 +93,16 @@ export class LocalStore{
         this.storeCollections(collections);
         this.updateCollections(collections);
     }
+    editCollection(oldName:string,newName:string){
+        var key = oldName.toLowerCase();
+        var newKey = newName.toLowerCase();
+        var collections = this._collections.getValue();
+        collections[newKey] = collections[key];
+        delete collections[key];
+        collections.collections.splice(collections.collections.indexOf(oldName),1, newName);
+        this.storeCollections(collections);
+        this.updateCollections(collections);
+    }
     addPicToCollection(pic, collection){
         collection = collection.toLowerCase();
         var collections = this._collections.getValue();
